59d2598acd3c7df6d2f3c7030f07349cbb65b8b9
Dom Hastings <dom111@users.noreply.github.com>
2021-01-14 07:02:39 +0000

    Initial commit

---
+++ Land.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Land = void 0;
+const Unit_1 = require("@civ-clone/core-unit/Unit");
+class Land extends Unit_1.default {
+}
+exports.Land = Land;
+exports.default = Land;
+//# sourceMappingURL=Land.js.map
---
+++ README.md
@@ -0,0 +1,4 @@
+# base-unit-type-land
+
+This is a template repo.
+
