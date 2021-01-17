19f897b6d271d756b51ccc3130a954a400aa12aa
Dom Hastings <dom111@users.noreply.github.com>
2021-01-17 21:10:38 +0000

    Initial commit

---
+++ Land.d.ts
@@ -0,0 +1,3 @@
+import Unit from '@civ-clone/core-unit/Unit';
+export declare class Land extends Unit {}
+export default Land;
---
+++ Land.ts
@@ -0,0 +1,5 @@
+import Unit from '@civ-clone/core-unit/Unit';
+
+export class Land extends Unit {}
+
+export default Land;
